bf98ffafd8be8a6535a2761b6f8340f0573df6dd
hassan khaled <119671650+Hassan-Khaled-Hassan@users.noreply.github.com>
2024-12-28 23:58:47 +0200

    final edit 28/12

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,13 @@
-import withBundleAnalyzer from "@next/bundle-analyzer";
 
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "export", // Enables static export mode
+  // reactStrictMode: true,
+  // output: "export", // Enables static export mode
   images: {
     domains: ["res.cloudinary.com"],
-    unoptimized: true, // Ensures images work in static export
+    // unoptimized: true, // Ensures images work in static export
   },
 };
 
-export default bundleAnalyzer(nextConfig);
+export default nextConfig;
