fa8fec537aa154f881ad336df9092bd578a88aae
hassan khaled <119671650+Hassan-Khaled-Hassan@users.noreply.github.com>
2024-12-31 19:31:00 +0200

    final edit 31/12 third

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export", // Enables static export mode
+  // output: "export", // Enables static export mode
   images: {
     domains: ["res.cloudinary.com"],
      unoptimized: true, // Ensures images work in static export
